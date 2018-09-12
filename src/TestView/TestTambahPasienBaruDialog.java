7675416458045624e69c834e892830ca3f8d0ba1
admin <admin@8b>
2018-09-12 12:07:05 +0700

    ver 0.6

---
+++ src/TestView/TestTambahPasienBaruDialog.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TestView;
+
+import View.TambahPasisenBaruDialog;
+
+/**
+ *
+ * @author admin
+ */
+public class TestTambahPasienBaruDialog {
+    public static void main(String[] args) {
+        TambahPasisenBaruDialog test = new TambahPasisenBaruDialog();
+        test.setSize(500, 400);
+        test.setVisible(true);
+    }
+}
