8403d674019ba032a2aca5f51f5a65cf621e651b
admin <admin@13b>
2018-09-19 11:14:58 +0700

    ver 0.7

--- src/Model/AntrianPasien.java
+++ src/Model/AntrianPasien.java
@@ -12,7 +12,8 @@ package Model;
 public class AntrianPasien {
     private int tanggalAntrian, bulanAntrian, tahunAntrian; //Pendeklarasian variabel bertipe data integer
     private Klinik klinik; //Pendeklarasian variabel bertipe data Klinik 
-    private Pasien daftarPasien[]; // pendeklarasian array bertipe data pasien 
+    private Pasien daftarPasienAntri[]; // pendeklarasian array bertipe data pasien 
+    private AntrianPasien daftarAntrian[];
 
     public Klinik getKlinik() {//membuat method baru yang akan mereturn variabel bertipe data klinik
         return klinik;
@@ -23,10 +24,31 @@ public class AntrianPasien {
     }
 
     public Pasien[] getDaftarPasien() {//membuat method baru yang akan mereturn variabel bertipe data aray pasien
-        return daftarPasien;
+        return daftarPasienAntri;
     }
 
     public void setDaftarPasien(Pasien[] daftarPasien) {//membuat method baru yang digunakan untuk memasukkan data dari parameter daftarPasien kedalam variabel daftarPasien
-        this.daftarPasien = daftarPasien;
+        this.daftarPasienAntri = daftarPasien;
+    }
+    
+    
+    public void mendaftar(Pasien pasien, int tanggal, int bulan, int tahun){
+    
+    }
+    
+    
+    public static AntrianPasien cariPasien (String noRM){
+        
+    }
+    
+    
+    public static Pasien cariPasien(String noRM, int tanggal, int bulan, int tahun){
+        
+    }
+    
+    
+    public static void buatAntrian ( int tanggal, int bulan, int tahun, Klinik klinik){
+        
     }
 }
+    
